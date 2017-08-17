553d440e14ef53dc8566305fd5883b8879c258f2
ogomb <mbogolewis@gmail.com>
2017-08-17 08:37:08 +0300

    edit __init__.py to include user registration

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
+import os
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-
-# local import
+from flask import session, request
+from flask import jsonify, abort, make_response
 from instance.config import app_config
 
-# initialize sql-alchemy
+
 db = SQLAlchemy()
 
-from flask import request, jsonify, abort
 
 def create_app(config_name):
-    from app.models import User
+    """ creating a flask app."""
+    from app.models import User, Bucketlist, Item
 
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
@@ -18,30 +19,31 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
-    @app.route('/signup/', methods=['POST','GET'])
-    def signup():
-        if request.method == 'POST':
-            username = str(request.data.get('username', ''))
-            email = str(request.data.get('email', ''))
-            password = str(request.data.get('password', ''))
-
-            checkusername = User.query.filter_by(username =username).first()
-            checkemail = User.query.filter_by(email=email).first()
-            if not checkusername and not checkemail:
-                user = User(username,email,password)
+    @app.route('/auth/register', methods=['POST', 'GET'])
+    def register():
+        """ register route."""
+        user = User.query.filter_by(username=request.data['username']).first()
+        if not user:
+            try:
+                data = request.data
+                email = data['email']
+                password = data['password']
+                username = data['username']
+                user = User(email=email, password=password, username=username)
                 user.save()
-                response = jsonify({
-                    'id' : user.id,
-                    'username' : user.username,
-                    'email' : user.email,
-                    'password' : user.password
-                    })
-                response.status_code =200
-                return response
-            else:
-                response = jsonify({'message': 'username or email already exists'})
-                response.status_code =301
-                return response
+
+                response = {'message': 'Registered'}
+                return make_response(jsonify(response)), 201
+            except Exception as e:
+                response = {
+                    'message': str(e)
+                }
+                return make_response(jsonify(response)), 401
+        else:
+            response = {
+                'message': 'User exists'
+            }
+            return make_response(jsonify(response)), 202
     @app.route('/login/', methods=['POST', 'GET'])
     def login():
         if request.method == 'POST':
