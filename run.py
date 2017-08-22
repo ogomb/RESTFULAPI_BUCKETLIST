fb778f83891c91a64d1c0c9adc40f310b3796b0d
Ogomb <mbogolewis@gmail.com>
2017-08-22 14:13:44 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,4 +6,6 @@ config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run()
