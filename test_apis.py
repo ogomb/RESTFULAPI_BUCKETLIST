8447c47be076cdb1f3f264524d61490000567fca
Ogomb <mbogolewis@gmail.com>
2017-08-25 23:24:09 +0300

    Update test_apis.py

--- test_apis.py
+++ test_apis.py
@@ -25,47 +25,6 @@ class APITest(unittest.TestCase):
 
         self.assertEqual(result['message'], 'Registered')
         self.assertEqual(response.status_code, 201)
-    def test_empty_signup(self):
-        """Test user signup."""
-        response = self.client().post('/auth/register', data={'username':'','email':'','password':''})
-        result = json.loads(response.data.decode())
-
-        self.assertEqual(result['message'], 'You can not submit null values!')
-        self.assertEqual(response.status_code, 201)
-    def test_signup_characters(self):
-        """Test user signup."""
-        response = self.client().post('/auth/register', data={'username':'lw','email':'mbogo@gmail.com','password':'344234'})
-        result = json.loads(response.data.decode())
-        
-
-        self.assertEqual(result['message'], 'username must be more than 8 characters')
-        self.assertEqual(response.status_code, 201)
-    def test_password_characters(self):
-        """Test user signup."""
-        response = self.client().post('/auth/register', data={'username':'lewis','email':'mbogo@gmail.com','password':'34'})
-        result = json.loads(response.data.decode())
-        
-
-        self.assertEqual(result['message'], 'password must be more than 8 characters')
-        self.assertEqual(response.status_code, 201)
-    def test_email_characters(self):
-        """Test user signup."""
-        response = self.client().post('/auth/register', data={'username':'lewis','email':'mbogogmail.com','password':'34654'})
-        result = json.loads(response.data.decode())
-        
-
-        self.assertEqual(result['message'], 'email has a bad format')
-        self.assertEqual(response.status_code, 201)
-    def test_username_characters(self):
-        """Test user signup."""
-        response = self.client().post('/auth/register', data={'username':'//lewis//','email':'mbogo@gmail.com','password':'34654'})
-        result = json.loads(response.data.decode())
-        
-
-        self.assertEqual(result['message'], 'username can not contain special characters')
-        self.assertEqual(response.status_code, 201)
-    
-    
     def test_user_exists(self):
         """Test if the user already exists."""
         response = self.client().post('/auth/register', data=self.user)
@@ -90,7 +49,7 @@ class APITest(unittest.TestCase):
         response = self.client().post('/auth/login', data=userDoesNotExist)
         result = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 401)
-        self.assertEqual(result['message'], 'username or password is incorrect')
+        self.assertEqual(result['message'], 'credentials are invalid')
 
     def user_registration(self, username="proffesor", email="prof@gmail.com", password="prof"):
         """A simple method to signup a user to be used in all the test cases that follow. """
@@ -100,64 +59,6 @@ class APITest(unittest.TestCase):
         """A simple method to login a user to be used in all the test cases that follow. """
         login_details = {'username':username, 'password':password}
         return self.client().post('/auth/login', data=login_details)
-    def test_user_logout_with_token(self):
-        """Test user logout."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().get(
-            '/auth/logout',
-            headers=dict(Authorization="Bearer "+token))
-
-        self.assertIn('you are logged out', str(response.data))
-        self.assertEqual(response.status_code, 200)
-    def test_user_logout_without_token(self):
-        """Test user logout."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().get(
-            '/auth/logout',
-            headers=dict(Authorization="Bearer "))
-
-        self.assertIn('you were not logged in', str(response.data))
-        self.assertEqual(response.status_code, 401)
-    def test_user_changepassword(self):
-        """Test user change password."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().post(
-            '/auth/reset_password',
-            headers=dict(Authorization="Bearer "+token),
-            data={'changepassword':'r3284712'})
-
-        self.assertIn('the password has changed', str(response.data))
-        self.assertEqual(response.status_code, 200)
-    def test_user_notoken_changepassword(self):
-        """Test user change password."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().post(
-            '/auth/reset_password',
-            headers=dict(Authorization="Bearer "),
-            data={'changepassword':''})
-
-        self.assertIn('No token provided', str(response.data))
-    
-    def test_user_changenullpassword(self):
-        """Test user change password."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().post(
-            '/auth/reset_password',
-            headers=dict(Authorization="Bearer "+token),
-            data={'changepassword':None})
-
-        self.assertIn('password has not changed', str(response.data))
-        
 
     def test_new_bucket_list(self):
         """Test the creation of a bucketlist."""
@@ -170,23 +71,6 @@ class APITest(unittest.TestCase):
             data=self.bucketlist)
         self.assertEqual(response.status_code, 201)
         self.assertIn('travel to taiwan', str(response.data))
-    def test_duplicate_bucket_list(self):
-        """Test the creation of a bucketlist."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().post(
-            '/bucketlists/',
-            headers=dict(Authorization="Bearer "+token),
-            data=self.bucketlist)
-        response2 = self.client().post(
-            '/bucketlists/',
-            headers=dict(Authorization="Bearer "+token),
-            data=self.bucketlist)
-        self.assertEqual(response2.status_code, 201)
-        self.assertIn('bucketlist with that name exists', str(response2.data))
-
-    
 
     def test_get_available_bucketlist(self):
         """Test the getting of the available bucketlist for a user."""
@@ -204,54 +88,7 @@ class APITest(unittest.TestCase):
             headers=dict(Authorization="Bearer "+token),)
         self.assertEqual(res.status_code, 200)
         #self.assertIn('travel to taiwan', str(res.data))
-    def test_querry_available_bucketlist(self):
-        """Test the querry of the available bucketlist for a user."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().post(
-            '/bucketlists/',
-            headers=dict(Authorization="Bearer "+token),
-            data=self.bucketlist)
-        self.assertEqual(response.status_code, 201)
 
-        res = self.client().get(
-            '/bucketlists/?q=lewis',
-            headers=dict(Authorization="Bearer "+token))
-        
-        self.assertIn('Bucketlist not found', str(res.data))
-    def test_getting_available_bucketlist(self):
-        """Test the querry of the available bucketlist for a user."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().post(
-            '/bucketlists/',
-            headers=dict(Authorization="Bearer "+token),
-            data=self.bucketlist)
-        self.assertEqual(response.status_code, 201)
-
-        res = self.client().get(
-            '/bucketlists/',
-            headers=dict(Authorization="Bearer "+token))
-        
-        self.assertIn('1', str(res.data))
-    def test_getting_available_bucketlist_no_token(self):
-        """Test the querry of the available bucketlist for a user."""
-        self.user_registration()
-        logged = self.login_the_user()
-        token = json.loads(logged.data.decode())['token']
-        response = self.client().post(
-            '/bucketlists/',
-            headers=dict(Authorization="Bearer "+token),
-            data=self.bucketlist)
-        self.assertEqual(response.status_code, 201)
-
-        res = self.client().get(
-            '/bucketlists/',
-            headers=dict(Authorization="Bearer "))
-        
-        self.assertIn('token not provided!', str(res.data))
     def test_get_bucketlist_having_an_id(self):
         """Testing getting a specific bucketlist based on the id."""
         self.user_registration()
@@ -314,12 +151,11 @@ class APITest(unittest.TestCase):
             '/bucketlists/{}'.format(results['id']),
             headers=dict(Authorization="Bearer "+token),)
         self.assertEqual(res.status_code, 200)
-        self.assertIn('The bucketlist is deleted', str(res.data))
+
         result = self.client().get(
             '/bucketlists/1',
             headers=dict(Authorization="Bearer "+token))
-        self.assertEqual(result.status_code, 200)
-        self.assertIn('The bucketlist does not exist', str(result.data))
+        self.assertEqual(result.status_code, 404)
     def test_create_item(self):
         """Test creating an item for a bucketlist."""
         self.user_registration()
