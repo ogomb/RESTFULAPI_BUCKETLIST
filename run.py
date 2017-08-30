46c403956c16778bd4fc0dc65b4b2f0d391aff44
Ogomb <lewis.mbogo@andela.com>
2017-08-30 22:58:13 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 import os
 
-from app import create_app
-
-config_name = os.getenv('APP_SETTINGS') # config_name = "development"
-app = create_app(config_name)
+from app import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    
+    app.run()
